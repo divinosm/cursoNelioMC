068584b91ea5548ab6d95d6c7c671b45e3d62912
divino miranda <divinosmiranda@hotmail.com>
2022-08-20 10:14:58 -0400

    inserindo variavel java

---
+++ src/main/java/br/gov/mt/intermat/projeto03/config/ProdConfig.java
@@ -0,0 +1,29 @@
+package br.gov.mt.intermat.projeto03.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.gov.mt.intermat.projeto03.domain.service.DbService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+    @Autowired
+    private DbService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+    @Bean
+    public boolean InstantiateDatabase() throws ParseException{
+        if (!"create".equals(strategy)){
+             return false;
+        }
+        dbService.instatiateTestDatabase();
+        return true;
+    }
+}
