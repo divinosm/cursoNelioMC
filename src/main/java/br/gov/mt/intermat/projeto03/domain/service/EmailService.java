ce8cef67094379019f09ef974e00df45286ddca3
divino miranda <divinosmiranda@hotmail.com>
2022-11-05 16:08:27 -0400

    email html

--- src/main/java/br/gov/mt/intermat/projeto03/domain/service/EmailService.java
+++ src/main/java/br/gov/mt/intermat/projeto03/domain/service/EmailService.java
@@ -1,11 +1,16 @@
 package br.gov.mt.intermat.projeto03.domain.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.gov.mt.intermat.projeto03.domain.model.Pedido;
 
 public interface EmailService {
+// para email normal (texto plano) 
     void sendOrderConfirmationEmail(Pedido obj);
-
 	void sendEmail(SimpleMailMessage msg);
+// para email html
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 }
--- src/main/java/br/gov/mt/intermat/projeto03/domain/service/SmtpEmailService.java
+++ src/main/java/br/gov/mt/intermat/projeto03/domain/service/SmtpEmailService.java
@@ -1,16 +1,22 @@
 package br.gov.mt.intermat.projeto03.domain.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService{
 
     // PARA INSTANCIAR COM TODOS DADOS DO EMAIL JÁ DECLARADOS PROFILE
     @Autowired
     private MailSender mailSender;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
@@ -20,6 +26,14 @@ public class SmtpEmailService extends AbstractEmailService{
 		LOG.info("iniciando envio de email...");
 		mailSender.send(msg);
 		LOG.info("Email enviado");
+    }
+
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+      LOG.info("iniciando envio de email html...");
+      javaMailSender.send(msg);
+      LOG.info("Email enviado");
     }
     
 }
